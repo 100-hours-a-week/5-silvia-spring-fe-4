35199c9267ee42ea15c4a5f9f6ee5c958612fb1a
moolmin <zzmzzm0065@gmail.com>
2024-05-17 11:32:10 +0900

    Refactoring (HOC)

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import HelperMessage from './HelperMessage';
 
-// 공통 InputField 컴포넌트
+// Common InputField component
 const InputField = ({ type, value, onChange, placeholder }) => {
     return (
         <input
@@ -14,27 +14,24 @@ const InputField = ({ type, value, onChange, placeholder }) => {
     );
 };
 
-
 const getFieldStyle = (error, helperText) => {
     return { marginBottom: error || helperText ? '0px' : '22px' };
 };
-
 
 const isValidEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 };
 
-
 const createInputField = (type, defaultLabel, defaultHelperText, additionalValidation) => {
-    return ({ value, onChange, placeholder, label = defaultLabel, error, labelStyle }) => {
+    return ({ label = defaultLabel, error, labelStyle, ...rest }) => {
         const [helperText, setHelperText] = useState(defaultHelperText);
 
         useEffect(() => {
-            if (!value) {
+            if (!rest.value) {
                 setHelperText(defaultHelperText);
             } else if (additionalValidation) {
-                const validationMessage = additionalValidation(value);
+                const validationMessage = additionalValidation(rest.value);
                 if (validationMessage) {
                     setHelperText(validationMessage);
                 } else {
@@ -43,7 +40,7 @@ const createInputField = (type, defaultLabel, defaultHelperText, additionalValid
             } else {
                 setHelperText('');
             }
-        }, [value]);
+        }, [rest.value]);
 
         const fieldStyle = getFieldStyle(error, helperText);
 
@@ -52,9 +49,8 @@ const createInputField = (type, defaultLabel, defaultHelperText, additionalValid
                 <div className="Text18" style={labelStyle}>{label}</div>
                 <InputField
                     type={type}
-                    value={value}
-                    onChange={onChange}
-                    placeholder={placeholder || `${label}을(를) 입력하세요`}
+                    placeholder={rest.placeholder || `${label}을(를) 입력하세요`}
+                    {...rest}
                 />
                 {(error || helperText) && <HelperMessage text={error || helperText} />}
             </div>
@@ -62,14 +58,13 @@ const createInputField = (type, defaultLabel, defaultHelperText, additionalValid
     };
 };
 
-
-// 각 입력 필드 컴포넌트 생성
+// Creating input field components
 const EmailInputField = createInputField("email", "이메일", "*이메일을 입력하세요.", (value) => !isValidEmail(value) && "*올바른 이메일 주소 형식을 입력해주세요");
 const PasswordInputField = createInputField("password", "비밀번호", "*비밀번호를 입력해주세요.");
 const PasswordConfirmInputField = createInputField("password", "비밀번호 확인", "*비밀번호 확인을 입력해주세요.");
 const NicknameInputField = createInputField("text", "", "*닉네임을 입력해주세요.", (value) => value.length > 10 ? "*닉네임은 최대 10자까지 작성 가능합니다." : "");
 
-// ProfileInputField 컴포넌트는 파일 업로드 특성 때문에 별도로 생성
+// ProfileInputField component is created separately due to the file upload nature
 const ProfileInputField = ({ profileImageError, profileImagePreview, handleProfileImageChange, label = "프로필 사진", labelStyle }) => {
     const [helperText, setHelperText] = useState('*프로필 사진을 추가해주세요.');
 
